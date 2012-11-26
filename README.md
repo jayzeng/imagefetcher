54b310029d3ddacc89c45f0f77a9c97bdd978ded
Jay Zeng <jayzeng@jay-zeng.com>
2012-11-25 17:21:44 -0800

    * Added package.json * Added a "how to run section" in readme.md * Uncommented debug code

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ search result and show it to everyone connected to the webpage.
 - socket.io and node.js for broadcasting the images
 - jQuery for DOM manipulation
 - jsonp for cross-site request to Google API
+
+How to run:
+- git clone git@github.com:jayzeng/imagefetcher.git
+- npm install
+- node server.js
+- open http://localhost:8080
--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
     res.end(fs.readFileSync(__dirname + '/index.html'));
 }).listen(8080, function() {
-    //console.log('Listening at: http://localhost:8080');
+    console.log('Listening at: http://localhost:8080');
 });
 
 socketio.listen(server).of('/image').on('connection', function (socket) {
     socket.on('image', function (msg) {
-        //console.log('Message Received: ', msg);
+        console.log('Message Received: ', msg);
         socket.broadcast.emit('image', msg);
     });
 });
