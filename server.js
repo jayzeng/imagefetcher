fc7c9a1378e7a50289436dab000a7903e00e745b
Jay Zeng <jayzeng@jay-zeng.com>
2013-02-17 20:41:10 -0800

    * Adjust port to be proc env or 80

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var fs = require('fs')
     , http = require('http')
+    , port = process.env.PORT || 80
     , socketio = require('socket.io');
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
     res.end(fs.readFileSync(__dirname + '/index.html'));
-}).listen(8080, function() {
-    console.log('Listening at: http://localhost:8080');
+}).listen(port, function() {
+    console.log('Listening at: http://localhost:' + port);
 });
 
 socketio.listen(server).of('/image').on('connection', function (socket) {
